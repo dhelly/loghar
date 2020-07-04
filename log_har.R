7ad94ccc199818411a834eebd115d8979b47b996
Jaqueline Fernandes <dhelly@gmail.com>
2020-07-04 11:35:29 -0300

    script extract data form .har file

---
+++ log_har.R
@@ -0,0 +1,30 @@
+#install.packages("HARtools")
+#install.packages("rjson")
+
+#load library
+library(HARtools)
+library(rjson)
+
+
+#read file .har
+har <- readHAR(har = file.choose())
+
+#number of entries
+size <- length(har[["log"]][["entries"]])
+
+#url composity
+endereco <- "youtube-dl -v https://player.vimeo.com/video/"
+ref <- "--referer url"
+
+dados <- c()
+
+for (i in seq(1:size)) {
+  fileLine <- har[["log"]][["entries"]][[i]][["response"]][["content"]][["text"]]
+  videoid <- fromJSON(fileLine)$video$vimeoid
+  videoTitle <- fromJSON(fileLine)$video$title
+  line <- paste(paste(endereco, videoid, sep = ""), paste(ref), paste("-o ", "'", videoTitle, ".mp4", "'", sep = ""), sep = " ")
+  dados <- c(dados, line)
+}
+
+#write bash file
+write(dados, "file.sh")
